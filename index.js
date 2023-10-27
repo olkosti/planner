e2ac8044532ca5f4b0053cfe9811d879ba354957
unknown <kov1606@gmail.com>
2023-10-28 01:47:49 +0300

    исправлен баг Нет задач

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const buttonAdd = document.querySelector(".button__add");
 const buttonClean = document.querySelector(".button__clean");
 const taskList = document.querySelector(".task__list");
 const notTask = document.querySelector(".not-task");
+const nottaskContainer = document.querySelector(".nottask__container");
 
+const pNotTask = document.createElement("p");
+pNotTask.className = "not-task";
+pNotTask.textContent = "Нет задач";
+nottaskContainer.appendChild(pNotTask);
 
 //Добавляем задачу в список задач при клике на кнопку
 buttonAdd.addEventListener ('click', () => {
-    let task = taskElement.value;
-    if (task !== "") {   
+    let task = taskElement.value;    
+    if (task.trim() !== "") {   
 // добавляем разметку для добавления задачи в список
-    notTask.remove();
+taskList
+    pNotTask.classList.add('inactive');
     const  divTaskItem = document.createElement("div");
     divTaskItem.className = "task__item";
     const  pTaskText = document.createElement("p");
@@ -30,7 +36,9 @@ buttonAdd.addEventListener ('click', () => {
 });
 
 buttonClean.addEventListener ('click', () => {
-    taskList.innerHTML = `<p class="not-task">Нет задач</p>`;
+    taskList.innerHTML = "";
+    pNotTask.classList.remove('inactive');    
     buttonClean.disabled = true;
     buttonClean.className = "button__clean";
 });
+
